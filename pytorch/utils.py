d6b614d002c0b96020005494ecaf12fa875762c0
renyiryry <49887303+renyiryry@users.noreply.github.com>
2019-05-08 16:13:42 -0400

    Update utils.py

--- pytorch/utils.py
+++ pytorch/utils.py
@@ -10,6 +10,7 @@ def kfac_update(data_, params):
     
     m = params['m']
     i = params['i']
+    inverse_update_freq = params['inverse_update_freq']
     
     # KFAC matrices
     G1_ = 1/m * a1.grad.t() @ a1.grad
