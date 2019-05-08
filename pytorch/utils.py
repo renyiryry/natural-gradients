dd99d0f2ec2bacafbe09fd34e37db70703af8faa
renyiryry <49887303+renyiryry@users.noreply.github.com>
2019-05-08 15:50:50 -0400

    Create utils.py

---
+++ pytorch/utils.py
@@ -0,0 +1,28 @@
+def kfac_update():
+    # KFAC matrices
+    G1_ = 1/m * a1.grad.t() @ a1.grad
+    A1_ = 1/m * X_mb.t() @ X_mb
+    G2_ = 1/m * a2.grad.t() @ a2.grad
+    A2_ = 1/m * h1.t() @ h1
+    G3_ = 1/m * z.grad.t() @ z.grad
+    A3_ = 1/m * h2.t() @ h2
+
+    G_ = [G1_, G2_, G3_]
+    A_ = [A1_, A2_, A3_]
+
+    # Update running estimates of KFAC
+    rho = min(1-1/i, 0.95)
+
+    for k in range(3):
+        A[k] = rho*A[k] + (1-rho)*A_[k]
+        G[k] = rho*G[k] + (1-rho)*G_[k]
+
+    # Step
+    for k in range(3):
+        # Amortize the inverse. Only update inverses every now and then
+        if (i-1) % inverse_update_freq == 0:
+            A_inv[k] = (A[k] + eps*torch.eye(A[k].shape[0])).inverse()
+            G_inv[k] = (G[k] + eps*torch.eye(G[k].shape[0])).inverse()
+
+        delta = G_inv[k] @ model.W[k].grad.data @ A_inv[k]
+        model.W[k].data -= alpha * delta
