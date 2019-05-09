5605ff9edf5ad03be5c52e7a17e6b411db6b4c86
renyiryry <49887303+renyiryry@users.noreply.github.com>
2019-05-09 11:42:02 -0400

    Update train.py

--- pytorch/train.py
+++ pytorch/train.py
@@ -172,6 +172,9 @@ for i in range(1, max_iter):
     print('loss: ', loss)
     
     loss.backward()
+    
+    print('model.W[1]: ', model.W[1])
+    print('model.W[1].grad.data: ', model.W[1].grad.data)
 
     if (i-1) % 100 == 0:
         
