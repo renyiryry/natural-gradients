4793293381b7d72bfd220b3c15ea2c8fc8b1d05d
renyiryry <49887303+renyiryry@users.noreply.github.com>
2019-05-10 10:49:26 -0400

    Update train.py

--- pytorch/train.py
+++ pytorch/train.py
@@ -161,7 +161,7 @@ if params['algorithm'] == 'kfac':
 elif params['algorithm'] == 'SMW-Fisher':
 #     N2 = 64
     
-    lambda = 1e-2
+    lambda_ = 1e-2
 #     lambda_ = 0.2
 #     lambda_ = 0.5
 #     lambda_ = 1
