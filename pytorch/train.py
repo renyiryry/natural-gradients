cfc8498aa3989ef6c63a3d1790be1cfec264c3b0
renyiryry <49887303+renyiryry@users.noreply.github.com>
2019-05-11 11:48:17 -0400

    Update train.py

--- pytorch/train.py
+++ pytorch/train.py
@@ -333,7 +333,7 @@ for i in range(1, max_iter):
 #         print(t_mb)
         
         print(f'Iter-{i-1}; Loss: {loss:.3f}')
-        if algorithm == 'SMW-Fisher':
+        if algorithm == 'SMW-Fisher' or algorithm == 'SMW-Fisher-momentum':
             lambda_ = params['lambda_']
             print('lambda = ', lambda_)
             print('\n')
