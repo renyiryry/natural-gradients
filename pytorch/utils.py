557a7b713cdd2ea924a849ab8df45ba29390dc1b
renyiryry <49887303+renyiryry@users.noreply.github.com>
2019-05-14 18:30:38 -0400

    Update utils.py

--- pytorch/utils.py
+++ pytorch/utils.py
@@ -1110,7 +1110,13 @@ def SMW_Fisher_update(data_, params):
     
     
 #     delta = compute_J_transpose_V_backp(hat_v, model, X_mb[N2_index], t_mb[N2_index], params)
-    delta = compute_J_transpose_V_backp(hat_v, data_, params)
+    if algorithm == 'SMW-Fisher':
+        delta = compute_J_transpose_V_backp(hat_v, data_, params)
+    elif algorithm == 'SMW-Fisher-momentum':
+        delta = compute_J_transpose_V(hat_v, data_, params)
+    else:
+        print('Error!')
+        sys.exit()
 
 #     print('test delta')
 #     delta = model_grad
