f652ead160596f29b8199141a9dec39f7c4ecdaa
renyiryry <49887303+renyiryry@users.noreply.github.com>
2019-05-14 15:53:20 -0400

    Update utils.py

--- pytorch/utils.py
+++ pytorch/utils.py
@@ -269,8 +269,10 @@ def compute_J_transpose_V_backp(v, data_, params):
     
     for l in range(len(v_2)):
         v_2[l] = v[l]
+        
+    print('v_2:', v_2)
     
-    loss, a, h = model.forward(X_mb, t_mb, v)
+    loss, a, h = model.forward(X_mb, t_mb, v_2)
 
     model = get_model_grad_zerod(model)
     
