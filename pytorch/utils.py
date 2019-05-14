6840534aa119d9a598598d625f8ba7886407e817
renyiryry <49887303+renyiryry@users.noreply.github.com>
2019-05-14 16:28:07 -0400

    Update utils.py

--- pytorch/utils.py
+++ pytorch/utils.py
@@ -987,24 +987,7 @@ def update_lambda(p, data_, params):
     
 #     print('time for update lambda 1/2: ', time.time() - start_time)
 
-     #########################
-#     mnist = data_['mnist']
-#     N1 = params['N1']
-    
-#     import torch
-    
-#     X_mb, t_mb = mnist.train.next_batch(N1)
-#     X_mb, t_mb = torch.from_numpy(X_mb), torch.from_numpy(t_mb).long()
 
-    # Forward
-    
-#     v = 1 / len(X_mb) * torch.ones(len(X_mb))
-#     loss, a, h = model.forward(X_mb, t_mb, v)
-
-#     model = get_model_grad_zerod(model)
-    
-#     loss.backward()
-#     print('test 11: 03 2')
         
     ll_chunk = get_new_loss(model, p, X_mb, t_mb)
     
@@ -1063,9 +1046,9 @@ def update_lambda(p, data_, params):
         rho = (oldll_chunk - ll_chunk) / denom
         
     
-#     print('oldll_chunk - ll_chunk: ', oldll_chunk - ll_chunk)
-#     print('denom: ', denom)
-#     print('rho: ', rho)
+        print('oldll_chunk - ll_chunk: ', oldll_chunk - ll_chunk)
+        print('denom: ', denom)
+        print('rho: ', rho)
         
     
     
