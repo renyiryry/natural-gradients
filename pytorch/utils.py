5a9fca46c6c87f975c8ae628764a88676df4b299
renyiryry <49887303+renyiryry@users.noreply.github.com>
2019-05-14 16:34:32 -0400

    Update utils.py

--- pytorch/utils.py
+++ pytorch/utils.py
@@ -1141,7 +1141,7 @@ def SMW_Fisher_update(data_, params):
     
 #     print('time for solve linear system: ', start_time - time.time())
     
-#     print('hat_v: ', hat_v)
+    print('hat_v: ', hat_v)
     
 #     print('torch.mean(hat_v): ', torch.mean(hat_v))
     
