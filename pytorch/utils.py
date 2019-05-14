8dafad732d73d2ee311faa241ea73acf5bf2afe0
renyiryry <49887303+renyiryry@users.noreply.github.com>
2019-05-14 10:11:57 -0400

    Update utils.py

--- pytorch/utils.py
+++ pytorch/utils.py
@@ -776,6 +776,9 @@ def update_lambda(p, data_, params):
      #########################
     mnist = data_['mnist']
     N1 = params['N1']
+    
+    import torch
+    
     X_mb, t_mb = mnist.train.next_batch(N1)
     X_mb, t_mb = torch.from_numpy(X_mb), torch.from_numpy(t_mb).long()
 
