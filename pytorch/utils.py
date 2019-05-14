314aebc760148a46d780addd1fdfbf2016493759
renyiryry <49887303+renyiryry@users.noreply.github.com>
2019-05-14 10:24:59 -0400

    Update utils.py

--- pytorch/utils.py
+++ pytorch/utils.py
@@ -166,7 +166,7 @@ def compute_J_transpose_V_backp(v, data_, params):
     model = get_model_grad_zerod(model)
     
 #     loss.backward()
-    weighted_Loss.backward()
+    weighted_loss.backward()
     print('test 10:16')
 
 
