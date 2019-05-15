b8b0fcfdeccf81c47ece40219c9f15dd8d41b991
renyiryry <49887303+renyiryry@users.noreply.github.com>
2019-05-15 16:24:19 -0400

    Update utils.py

--- pytorch/utils.py
+++ pytorch/utils.py
@@ -1021,13 +1021,13 @@ def SMW_Fisher_update(data_, params):
         if i % inverse_update_freq == 0:
             D_t = get_D_t(data_, params)
             D_t_inv = D_t.inverse()
-            data_['D_tinv'] = D_t_inv
+            data_['D_t_inv'] = D_t_inv
         else:
             D_t_inv = data_['D_t_inv']
         
-        print('D_t_inv ', D_t_inv)
+#         print('D_t_inv ', D_t_inv)
 #         print('D_t_inv', D_t_inv.size())
-        print('v: ', v)
+#         print('v: ', v)
         
         hat_v = D_t_inv @ v
         
