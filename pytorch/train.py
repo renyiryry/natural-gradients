1c3dc3292a4405e5637ef51ea5ba3a247cf4ccc4
renyiryry <49887303+renyiryry@users.noreply.github.com>
2019-05-15 10:51:21 -0400

    Update train.py

--- pytorch/train.py
+++ pytorch/train.py
@@ -438,7 +438,7 @@ for i in range(int(max_epoch * iter_per_epoch)):
         model_grad.append(copy.deepcopy(model.W[l].grad))
     data_['model_grad'] = model_grad
     
-#     print('model_grad[1] in train: ', model_grad[1])
+    print('model_grad[1] in train: ', model_grad[1])
     
     
     
