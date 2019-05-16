6c179df61073f4a2e053874c9911bfd83a705738
renyiryry <49887303+renyiryry@users.noreply.github.com>
2019-05-15 20:44:38 -0400

    Update utils.py

--- pytorch/utils.py
+++ pytorch/utils.py
@@ -316,11 +316,13 @@ def get_D_t(data_, params):
             
             print('permuted_a_grad_l.size(): ', permuted_a_grad_l.size())
             
-            print('(torch.from_numpy(np.kron(h[l] @ h[l].t(), np.ones(m_L, m_L)))).size()' (torch.from_numpy(np.kron(h[l] @ h[l].t(), np.ones(m_L, m_L)))).size())
+#             print('(torch.from_numpy(np.kron(h[l] @ h[l].t(), np.ones(m_L, m_L)))).size()' (torch.from_numpy(np.kron(h[l] @ h[l].t(), np.ones(m_L, m_L)))).size())
             
             print('(permuted_a_grad_l.t() @ permuted_a_grad_l).size()', (permuted_a_grad_l.t() @ permuted_a_grad_l).size())
             
-            D_t += torch.from_numpy(np.kron(h[l] @ h[l].t(), np.ones(m_L, m_L)))\
+            h_l_h_l_t = (h[l] @ h[l].t()).data.numpy()
+            
+            D_t += torch.from_numpy(np.kron(h_l_h_l_t, np.ones(m_L, m_L)))\
             * (permuted_a_grad_l.t() @ permuted_a_grad_l)
     else:
         print('Error!')
