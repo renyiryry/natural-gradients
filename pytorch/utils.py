33c47e91287b330fd3683f2906cec5b5132d6c49
renyiryry <49887303+renyiryry@users.noreply.github.com>
2019-05-16 10:59:24 -0400

    Update utils.py

--- pytorch/utils.py
+++ pytorch/utils.py
@@ -9,7 +9,7 @@ import copy
 def get_loss(model, x, t):
     z, _, _ = model.forward(x)   
     loss = F.cross_entropy(z, t, reduction = 'mean')
-    return loss.data.numpy()
+    return loss
 
 def get_acc(model, x, t):
     z, _ , _= model.forward(torch.from_numpy(x))
