3a6637b3fe172bb5976d9bac82bfa0aecfcf1fae
renyiryry <49887303+renyiryry@users.noreply.github.com>
2019-06-30 11:35:21 -0400

    Update utils.py

--- pytorch/utils.py
+++ pytorch/utils.py
@@ -1299,8 +1299,8 @@ def SMW_Fisher_update(data_, params):
         
     p = get_minus(delta, params)
     
-    print('get_dot_product(p, model_grad, params)')
-    print(get_dot_product(p, model_grad, params))
+#     print('get_dot_product(p, model_grad, params)')
+#     print(get_dot_product(p, model_grad, params))
     
   
 
