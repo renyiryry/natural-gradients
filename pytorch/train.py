fa483e6967210f082a74b9aec21d744e639a6ee6
renyiryry <49887303+renyiryry@users.noreply.github.com>
2019-07-27 13:28:27 -0400

    Update train.py

--- pytorch/train.py
+++ pytorch/train.py
@@ -472,6 +472,12 @@ for i in range(int(max_epoch * iter_per_epoch)):
         print('F.softmax(z, dim=0)')
         print(F.softmax(z, dim=0))
         
+        print('F.sum(F.softmax(z, dim=0), dim=0)')
+        print(F.sum(F.softmax(z, dim=0), dim=0))
+        
+        print('F.sum(F.softmax(z, dim=0), dim=1)')
+        print(F.sum(F.softmax(z, dim=0), dim=1))
+        
         data_['t_mb_pred'] = t_mb
     
         data_['a'] = a
